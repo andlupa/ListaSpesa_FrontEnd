39b248c8bfcbbf5ef79ffcb6efdd01670a306b62
andlupa <andlupa@gmail.com>
2026-08-16 19:03:00 +0200

    Aggiorna correzione errore apiurl prefisso /api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://listaspesa-backend.calmriver-b56a30b9.westeurope.azurecontainerapps.io',
+  apiUrl: 'https://listaspesa-backend.calmriver-b56a30b9.westeurope.azurecontainerapps.io/api',
   apiKey: 'ForzaInter'
 };
