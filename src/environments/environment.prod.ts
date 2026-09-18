a29b3cf72d619f68db3b8f854829c07cd10eb63e
andlupa <andlupa@gmail.com>
2026-09-18 17:35:04 +0200

    Update API endpoint to Render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://listaspesa-backend.calmriver-b56a30b9.westeurope.azurecontainerapps.io/api',
+  apiUrl: 'https://listaspesa-backend-ypqm.onrender.com/api',
   apiKey: 'ForzaInter'
 };
